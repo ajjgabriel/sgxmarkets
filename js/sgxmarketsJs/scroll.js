263c818f741ce57621c79828e3d9792979d51ff2
ajjgabriel@gmail.com <>
2013-09-03 05:30:29 +0000

    Fundamentals Done

---
+++ js/sgxmarketsJs/scroll.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+ 
+	// When #announcementScroll is clicked
+	jQuery('#announcementScroll').click(function(){
+		// Scroll down to 'catTopPosition'
+		jQuery('html, body').animate({scrollTop:0}, 'slow');
+		// Stop the link from acting like a normal anchor link
+		return false;
+	});
+
+ 
+  var fundamentalsPosition = jQuery('#Fundamentals').offset().top;
+	
+	// When #scroll is clicked
+	jQuery('#fundamentalScroll').click(function(){
+		// Scroll down to 'catTopPosition'
+		jQuery('html, body').animate({scrollTop:fundamentalsPosition}, 'slow');
+		// Stop the link from acting like a normal anchor link
+		return false;
+	});
+});
